2417ac42ef585ee2be31cb77ed136e5b251d82cf
eslamibrahim30 <eslam.ibrahim3082001@gmail.com>
2023-10-22 01:07:13 -0700

    Sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -7,7 +7,7 @@
 result = []
 ARGV.each do |arg|
     # skip if not integer
-    next if arg !~ /^[-+]?[0-9]+$/
+    next if arg !~ /^[-+]?\d+?(\.\d+)?$/
 
     # convert to integer
     i_arg = arg.to_i
